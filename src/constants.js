48ef4111cff51819599758212d2f0c1f26c2743c
magicbob31 <guibe310@gmail.com>
2024-07-01 16:41:08 +0200

    portfolio 2d

---
+++ src/constants.js
@@ -0,0 +1,28 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `Bienvenue sur mon profil <a href= "https://github.com/magicbob31">github</a>`,
+  tv: `Vous ne savez pas quoi regarder ? Quoi de mieux que d'essayer une appli maison <a href="https://moovieslib.netlify.app">moovieslib</a>`,
+  bed: `Mon lit`,
+  resume: `J'ai suivi une formation en developpement web et web mobile, ce qui
+          m'a permis d'acquerir des competences solides dans plusieurs langages
+          et technologies du domaine. <br />
+          <br />
+          Je maitrise le HTML, le JavaScript, Node.js, CSS, SCSS, React, ainsi
+          que le SQL. Fort de cette formation, je suis capable de concevoir et
+          developper des sites web et des applications mobiles modernes, en
+          utilisant des outils et frameworks contemporains. <br />
+          <br />
+          Mon expertise en React me permet de creer des interfaces utilisateur
+          dynamiques et reactives, tandis que mes connaissances en Node.js et
+          SQL me donnent la possibilite de gerer efficacement les aspects
+          backend et les bases de donnees. Je suis passionne par les nouvelles
+          technologies et toujours a l'affut des dernieres tendances dans le
+          domaine du developpement web. Je suis pret a relever de nouveaux defis
+          et a apporter mes competences a des projets innovants.`,
+  projects: `Mon CV`,
+  library: `Ma bibliotheque`,
+  "cs-degree": `diplome`,
+  "sofa-table": `sofa`,
+  exit: `Prochainement....`,
+};
